c0b4b9822963aa57cd82505b22af98be013b17d0
Ilya <kotel22@bk.ru>
2023-08-10 00:04:04 +0700

    Добавил папку Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,32 @@
+﻿// //Напишите программу которая 
+// 1/выводит случайное трехзначное число из отрезка
+// 2/ удаляет вторую цифру этого числа
+// 456-> 46
+// 782->72
+// 918->98
+
+// Используем функции из лекции
+
+int number = new Random().Next(10, 100); //получим рандомные числа от 10 до 100
+Console.WriteLine($"Случайное число из диапозона от 10 до 99 --> {number}");
+
+// int firstDigit = number / 10; //делим на 10 получаем 7,8 т.к. int используется поэтому выводится только 7, а это первое число
+// int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа {maxDigit}");
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа {maxDigit}");
+
+int MaxDigit(int num) // num = number
+{
+    int firstDigit = num / 10; //делим на 10 получаем 7,8 т.к. int используется поэтому выводится только 7, а это первое число
+    int secondDigit = num % 10;
+    // if (firstDigit > secondDigit) return firstDigit;
+    // else return 0;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
