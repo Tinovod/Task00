117edfae8835c11b9fff9158b9c787f8835d1bac
Ilya <kotel22@bk.ru>
2023-08-11 01:08:33 +0700

    Добавил папку Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// //Напишите программу которая 
+// 1/выводит случайное трехзначное число и на выходе показывает вторую 
+// цифру этого числа
+// 456-> 5
+// 782->8
+// 918->1
+
+// Используем функции из лекции
+
+int number = new Random().Next(100, 1000); //получим рандомные числа от 10 до 100
+Console.WriteLine($"Случайное число из диапозона от 100 до 999 --> {number}");
+
+int digit = Digit(number);
+Console.WriteLine(digit);
+
+int Digit(int num) // num = number название метода
+//  и параметр, или исходные данные
+{
+    int secondDigit = (num / 10) % 10;
+    int result = (secondDigit);
+    return result;
+}
