17cea51d3c1e7efc0144f4fc0878fbe819639cf5
Ilya <kotel22@bk.ru>
2023-08-11 01:09:33 +0700

    Добавил папку Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,24 @@
+﻿// //Напишите программу которая будет 
+// принимать на вход два числа и проверяет, 
+// является ли одно число квадратом другого
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+// Используем функции из лекции
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+bool result = MultiplicityTwoDigit(num1, num2);
+Console.WriteLine(result ? "Да" : "Нет");
+
+bool MultiplicityTwoDigit(int numb1, int numb2)
+{
+    return (numb1 * numb1 == numb2 || numb2 * numb2 == numb1);
+}
