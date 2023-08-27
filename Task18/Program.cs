b86a8611f4fef30ae15d6c40a142382c84ef9018
Ilya <kotel22@bk.ru>
2023-08-27 16:12:05 +0700

    Создал папку 18

---
+++ Task18/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход номер четверти
+// 
+// 2. выдает координаты четверти плоскости, в которой 
+// находится эта точка
+
+string RangeCoordinat(int quart)
+{
+    if (quart == 1) return x > 0 && y > 0;
+    if (quart == 2) return x < 0 && y > 0;
+    if (quart == 3) return x < 0 && y < 0;
+    if (quart == 4) return x > 0 && y < 0;
+    return null;
+}
+
+Console.WriteLine ("Введите номер четверти");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string rangeCoordinat = RangeCoordinat(quarter);
+string result = rangeCoordinat != null ? rangeCoordinat 
+                                                : "Введена некорректная четверть";
+Console.WriteLine(result);
