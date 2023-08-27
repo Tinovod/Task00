33e07c83c3cd40286084129a90e2d44f928e6dbe
Ilya <kotel22@bk.ru>
2023-08-27 23:49:07 +0700

    Добавил папку Массив

---
+++ Massivi/Program.cs
@@ -0,0 +1,34 @@
+﻿void SelectionSort(int[] array)
+{
+ for (int i = 0; i < array.Length - 1; i++)
+ {
+ int minPosition = i;
+ for (int j = i + 1; j < array.Length; j++)
+ {
+ if (array[j] < array[minPosition])
+ {
+ minPosition = j;
+ }
+ }
+ int temporary = array[i];
+ array[i] = array[minPosition];
+ array[minPosition] = temporary;
+ }
+}
+void SelectionSort(int[] array)
+{
+ for (int i = 0; i < array.Length - 1; i++)
+ {
+ int minPosition = i;
+ for (int j = i + 1; j < array.Length; j++)
+ {
+ if (array[j] < array[minPosition])
+ {
+ minPosition = j;
+ }
+ }
+ int temporary = array[i];
+ array[i] = array[minPosition];
+ array[minPosition] = temporary;
+ }
+}
