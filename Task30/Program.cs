a95b9d47acc86dbcef3c44c8f3b218575b4c609d
Ilya <kotel22@bk.ru>
2023-08-27 23:48:44 +0700

    Добавил папку 30

---
+++ Task30/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 30: Напишите программу, которая 
+// принимает выводит массив из 8 элементов 
+// заполненный нулями и единицами в случ порядке
+// [1,0,1,1,0,1,0]
+
+// int SumProiz(int num)
+// {
+//     int pro = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         // sum = sum + 1;
+//         pro *= i;
+//     }
+//     return pro;
+// }
+
+// Console.WriteLine("Введите натуральное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result = SumProiz(number);
+// Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
+
+
+int [] array = new int [8]; // 0 1 2 3 4 5 6 7 
+// int [] array = {43, 54, 52, 52,41,489, 14, 75}; // либо так можно записать
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(2);
+    Console.WriteLine(array[i]+ " ");
+    
+}
