4dc0ee394b15fe333c43a8572c57e6f5e2d69524
Ilya <kotel22@bk.ru>
2023-08-30 22:50:56 +0700

    Добавил задачу 09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,3 +9,24 @@
 
 int number = new Random().Next(10, 100); //получим рандомные числа от 10 до 100
 Console.WriteLine($"Случайное число из диапозона от 10 до 99 --> {number}");
+
+// int firstDigit = number / 10; //делим на 10 получаем 7,8 т.к. int используется поэтому выводится только 7, а это первое число
+// int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа {maxDigit}");
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа {maxDigit}");
+
+int MaxDigit(int num) // num = number
+{
+    int firstDigit = num / 10; //делим на 10 получаем 7,8 т.к. int используется поэтому выводится только 7, а это первое число
+    int secondDigit = num % 10;
+    // if (firstDigit > secondDigit) return firstDigit;
+    // else return 0;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
