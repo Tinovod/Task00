1dc6a309a794ce4a546e2de13c2cdf59c78509a0
Ilya <kotel22@bk.ru>
2023-08-31 23:44:50 +0700

    Решил задачу 29ДЗ

--- Task29DZ/Program.cs
+++ Task29DZ/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+void FillArrayRandom (int[] array)
+{
+
+ Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 34);
+
+}
+}
+
+void PrintDigit(int[] arr)
+ {
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write(" -> ");
+ }
+void PrintArray(int[] arr)
+ {
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+ }
+
+int[] arr = new int[8]; 
+
+FillArrayRandom(arr);
+ PrintDigit(arr);
+ PrintArray(arr);
