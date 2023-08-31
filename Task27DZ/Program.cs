988561cc3f419e0fdcda439943c221d971a749e8
Ilya <kotel22@bk.ru>
2023-08-31 23:15:30 +0700

    Решил задачу 27ДЗ

--- Task27DZ/Program.cs
+++ Task27DZ/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigit(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10; 
+        number /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int input = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(input);
+
+Console.WriteLine($"Сумма цифр числа {input} = {sumDigit}");
+
