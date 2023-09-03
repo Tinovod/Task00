c5b1cfa8034e3f9204857ae6852fd6a9f6eec346
Ilya <kotel22@bk.ru>
2023-09-04 00:42:14 +0700

    Добавил задачу 42

---
+++ Task42/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 42: Напишите программу, 
+// которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int DecimalToBin (int num)
+{
+    int res = 0;
+    int factor = 1;
+    while(num > 0)  
+    {
+    res += num % 2 * factor;
+    num /= 2;
+    factor *= 10;
+    }
+    return res;
+ }
+
+
+
+Console.WriteLine ("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int decimalToBin = DecimalToBin(num1);
+Console.WriteLine(decimalToBin);
